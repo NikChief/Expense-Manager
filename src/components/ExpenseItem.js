a9a77a8b582878fcda9bd490bda6285f5666bfc6
NikChief <niki3000-90@mail.ru>
2022-05-26 09:41:56 +0500

    ExpenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({expense}) {
-  const month = expense.date.toLocaleString('en-US', { month: 'long' });
-  const day = expense.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = expense.date.getFullYear();
+
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={expense.date}/>
       <div className='expense-item__description'>
         <h2>{expense.title}</h2>
         <div className='expense-item__price'>{expense.amount}</div>
