761e597dae788c4d339f94371b1b286546080d4a
NikChief <niki3000-90@mail.ru>
2022-05-29 20:21:04 +0500

    save data after reload page(add local.storage logic)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  const [expenses, setExpenses] = useState([
-    { id: 1, date: new Date(2022, 1, 4), title: 'Kids', amount: 120 },
-    { id: 2, date: new Date(2021, 3, 4), title: 'Car', amount: 140 },
-    { id: 3, date: new Date(2022, 3, 4), title: 'Teeth', amount: 100 },
-  ])
+  const initial = localStorage.getItem("expensesStorage") ? 
+    JSON.parse(localStorage.getItem("expensesStorage")).map(element => {
+      return {
+        ...element,
+        date: new Date(element.date)
+      }
+    }) : 
+    [
+      { id: 1, date: new Date(2022, 1, 11), title: 'House design', amount: 3100 },
+      { id: 2, date: new Date(2021, 2, 7), title: 'Car tools', amount: 1700 },
+      { id: 3, date: new Date(2022, 3, 15), title: 'Teeth', amount: 2450 },
+      { id: 4, date: new Date(2020, 5, 25), title: 'Party', amount: 2500 },
+      { id: 5, date: new Date(2021, 3, 12), title: 'Trip to the sea', amount: 5100 },
+      { id: 6, date: new Date(2022, 6, 4), title: 'Sport inventory', amount: 1200 },
+      { id: 7, date: new Date(2022, 11, 21), title: 'PS5 Games', amount: 370 }
+    ]
+
+  const [expenses, setExpenses] = useState(initial)
+
+  localStorage.setItem("expensesStorage", JSON.stringify([...expenses]));
 
   const addExpenseHandler = expense => {
     console.log('In App.js');
@@ -27,10 +42,10 @@ function App() {
 
   const extractUpgratedItem = (updatingId, updatingItemData) => {
     console.log(updatingId, updatingItemData);
-    const dateArray = updatingItemData.date.split('-').map(elementDate=>+elementDate)
+    const dateArray = updatingItemData.date.split('-').map(elementDate => +elementDate)
     setExpenses(prevExpenses => {
       return [...prevExpenses].map(expense => {
-        if (expense.id!==updatingId) {
+        if (expense.id !== updatingId) {
           return expense
         } else {
           return {
@@ -39,7 +54,7 @@ function App() {
             date: new Date(...dateArray),
             amount: updatingItemData.amount,
           }
-        } 
+        }
       })
     })
   }
